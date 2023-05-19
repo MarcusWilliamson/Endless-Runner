ca0f2826dd3e9be84a3f7cfe0a127012c0b02bb6
Marcus Williamson <hamawill@ucsc.edu>
2023-05-18 20:03:03 -0700

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,30 @@
+/*
+Main header
+
+
+Ideas:
+- Pinball moving upwards, hit with paddles around the map
+- Parachuting downwards avoiding obstacles (parachute, balloons, flower)
+- Platformer runner
+- Combat focused runner
+- Grid-based electrical circuit runner/maze endless runner
+- Horde endless runner
+- Conveyor belts
+- Swinging
+- Mapping keyboard keys to physical locations on map (running along keyboard)
+- Relay races
+- Going down slopes
+*/
+
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480,
+    scene: [Menu, Play],
+    fps: {
+        forceSetTimeOut: true,
+        target: 60
+    }
+}
+
+let game = new Phaser.Game(config);
---
+++ src/scenes/Menu.js
@@ -0,0 +1,26 @@
+class Menu extends Phaser.Scene {
+    constructor() {
+        super("menuScene");
+    }
+
+    create() {
+         // menu text configuration
+         let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '24px',
+            backgroundColor: '#F3B141',
+            color: '#000000',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+
+        // show menu text
+        this.add.text(game.config.width / 2, game.config.height / 2 - game.config.height / 15, 'Slimeblock Run', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width / 2, game.config.height / 2, 'Press SPACE to start', menuConfig).setOrigin(0.5);
+    }
+
+}
