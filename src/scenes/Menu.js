0a198c5b930fa62eb24ba909a257c0831194ac39
Marcus Williamson <hamawill@ucsc.edu>
2023-05-18 20:28:26 -0700

    Slime cube in play scene, spacebar

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -21,6 +21,15 @@ class Menu extends Phaser.Scene {
         // show menu text
         this.add.text(game.config.width / 2, game.config.height / 2 - game.config.height / 15, 'Slimeblock Run', menuConfig).setOrigin(0.5);
         this.add.text(game.config.width / 2, game.config.height / 2, 'Press SPACE to start', menuConfig).setOrigin(0.5);
+
+        // define keys
+        keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    }
+
+    update() {
+        if(Phaser.Input.Keyboard.JustDown(keySPACE)) {
+            this.scene.start("playScene");
+        }
     }
 
 }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -2,4 +2,21 @@ class Play extends Phaser.Scene {
     constructor() {
         super("playScene");
     }
+
+    preload() {
+        // load images
+        this.load.image('slime', './assets/slime.png');
+    }
+
+    create() {
+        // add slime (player)
+        this.playerSlime = new Slime(this, game.config.width / 15, game.config.height - game.config.height / 15, 'slime').setOrigin(0.5, 0);
+
+        // define keys
+        keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    }
+
+    update() {
+        this.playerSlime.update();
+    }
 }
