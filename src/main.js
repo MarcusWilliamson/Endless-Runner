c10ae7e75b12c83968ded56e4cca468ac5e58ff9
Marcus Williamson <hamawill@ucsc.edu>
2023-07-31 16:26:52 -0700

    Updated header again

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ but I couldn't get it to work, so I made my own with the getInactiveTile functio
 which checks an array of existing obstacles for an inactive obstacle it needs (platform or hazard), sets
 its new location and sets it as active, and returns it. If it doesn't find one it creates a new
 one. This way I don't constantly create and destroy new obstacles, or just let them pile up off screen.
+This function is called by spawnPlatform() and spawnHazard(), which are called in update().
 The game has a 2000s flash game art style and the gameplay is intuitive and quick. Although the game
 is a pretty simple, standard runner, I think it has something different in the obstacles that you can
 stand on for extra points.
